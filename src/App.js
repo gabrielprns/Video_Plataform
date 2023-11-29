2161d3323fdf3043479d6c1ce41b6d18a4a006bb
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2023-11-29 18:01:59 -0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Feed } from './pages/home';
+import { Box } from '@mui/material';
+import VideoDetail from './pages/VideoDetail';
+import ChannelDetail from './pages/ChannelDetail';
+import SearchFeed from './pages/SearchFeed';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //rotas
+    <Router>
+      <Box sx={{bgcolor: "#000"}}>
+        <Routes> 
+          <Route path="/" element={<Feed/>}></Route>
+          <Route path="/video/:id" element={<VideoDetail/>}></Route>
+          <Route path="/channel/:id" element={<ChannelDetail/>}></Route>
+          <Route path="/search/:searchTerm" element={<SearchFeed/>}></Route>
+          
+        </Routes>
+      </Box>
+    </Router>
+
+    
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export const Feed = ()=>{
+
+  return(
+    <Button variant="contained">Hello world</Button>
+  )
+
+}
