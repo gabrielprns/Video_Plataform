03daf3dfc7b89004b760dfd4eaecf9d9ca587628
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2023-12-07 22:55:11 -0300

    Project

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Stack} from '@mui/material'
+import { Link } from 'react-router-dom'
+import { logo } from "../until/constants"
 
 const NavBar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <Stack 
+    direction = "row" 
+    alignItens= "center" 
+    p={2} 
+    sx= {{position: "sticky", background: "#000", top: 0, justifyContent:"space-between"}}>
+
+      <Link to='/' style={{display:"flex", alignItems:"center"}}>
+        <img src={logo} alt="logo" height = {45} />
+      </Link>
+    </Stack>
   )
 }
 
