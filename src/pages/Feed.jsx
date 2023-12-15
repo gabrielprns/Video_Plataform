c57b466721acaf59cdf754ea0f6e027ee281b239
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2023-12-15 20:59:53 -0300

    Project

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Box, Typography, Stack} from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>
-      Feed
-    </div>
+    <Stack sx={{flexDirection:{sx: "column", md:'row'}}}>
+      
+      <Box sx={{height: {sx:"auto", md:"92vh"}, 
+      borderRight:"1px solid #3d3d3d", px: {sx: 0, md: 2}}}>
+
+        <Typography className = "copyright" variant='body2'
+        sx={{mt: 1.5, color:"#fff"}}>
+
+          Copyright @ 2023 Gabriel Pedro
+
+        </Typography>
+        
+      </Box>
+
+
+    </Stack>
+    
   )
 }
 
